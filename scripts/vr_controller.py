892f1c633e42803df581f44136484baf05c01bb8
Kuwamai <mitsumex333@gmail.com>
2019-06-14 02:06:55 +0900

    Edit: fragを用いてリセットした

--- scripts/vr_controller.py
+++ scripts/vr_controller.py
@@ -8,27 +8,29 @@ from geometry_msgs.msg import PoseStamped
 class Pose_pub:
     def __init__(self):
         self._sub_pos = rospy.Subscriber("controller_r", PoseStamped, self.pose_callback)
-        self.r = rospy.Rate(10)
+        self.reset_f = True
 
     def pose_callback(self, message):
-        rospy.loginfo(message.pose)
         self.pose = message.pose
+        rospy.loginfo(message.pose)
+
+        if self.reset_f:
+            self.reset_pose()
+            self.reset_f = False
+
+        else:
+            self.ik()
         
     def reset_pose(self):
         self.zero_pose = self.pose
 
     def ik(self):
-        while not rospy.is_shutdown():
-            rospy.loginfo(self.pose.position.x - self.zero_pose.position.x)
-            self.r.sleep()
+        rospy.loginfo(self.pose.position.x - self.zero_pose.position.x)
 
 if __name__ == '__main__':
     try:
         rospy.init_node('vr_controller')
         pose_pub = Pose_pub()
-        time.sleep(0.1)
-        pose_pub.reset_pose()
-        pose_pub.ik()
         rospy.spin()
 
     except rospy.ROSIntteruptException:
