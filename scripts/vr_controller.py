6fba39abfd179d781e297764c3d4fa1ef8345c99
Kuwamai <mitsumex333@gmail.com>
2019-06-15 01:57:47 +0900

    Fix: subscribeとikを非同期処理に

--- scripts/vr_controller.py
+++ scripts/vr_controller.py
@@ -8,51 +8,41 @@ import numpy as np
 class Pose_pub:
     def __init__(self):
         self._sub_pos = rospy.Subscriber("controller_r", PoseStamped, self.pose_callback)
+        self.zero_pose = rospy.wait_for_message("controller_r", PoseStamped).pose
         self.r = rospy.Rate(10)
-        self.reset_f = True
-        self.scale_fac = 0.5
-        self.z_offset = 0.05
+        self.scale_fac = 1.
+        self.offset = 0.1
         self.q = np.array([[0.0],
-                           [0.2],
-                           [2.5]])
+                           [np.pi/4],
+                           [np.pi/2]])
 
     def pose_callback(self, message):
         self.pose = message.pose
-        #rospy.loginfo(message.pose)
-
-        if self.reset_f:
-            self.reset_pose()
-            self.reset_f = False
-
-        else:
-            self.ik()
-        
-    def reset_pose(self):
-        self.zero_pose = self.pose
 
     def ik(self):
-        r_ref = np.array([[self.pose.position.x - self.zero_pose.position.x],
-                          [self.pose.position.y - self.zero_pose.position.y],
-                          [self.pose.position.z - self.zero_pose.position.z + self.z_offset]])
+        while not rospy.is_shutdown():
+            r_ref = np.array([[self.pose.position.x - self.zero_pose.position.x + self.offset],
+                              [self.pose.position.y - self.zero_pose.position.y + self.offset],
+                              [self.pose.position.z - self.zero_pose.position.z + self.offset]])
 
-        #rospy.loginfo(r_ref)
-        r_ref *= self.scale_fac
-        #rospy.loginfo(r_ref)
+            #rospy.loginfo(r_ref)
+            r_ref *= self.scale_fac
+            rospy.loginfo(r_ref)
 
-        r_ref_norm = np.linalg.norm(r_ref, ord=2)
-        
-        if r_ref_norm > 2.0:
-            rospy.loginfo("wawawa")
-            r_ref /= r_ref_norm
-            r_ref *= 1.99
+            r_ref_norm = np.linalg.norm(r_ref, ord=2)
+            
+            if r_ref_norm > 2.0:
+                rospy.loginfo("wawawa")
+                r_ref /= r_ref_norm
+                r_ref *= 1.99
 
-        for i in range(10):
-            r = self.fk(self.q)
-            self.q = self.q - np.linalg.inv(self.J(self.q)).dot((r - r_ref))
+            for i in range(10):
+                r = self.fk(self.q)
+                self.q = self.q - np.linalg.inv(self.J(self.q)).dot((r - r_ref))
 
-        rospy.loginfo(self.q.T)
-        rospy.loginfo(r_ref - r)
-        self.r.sleep()
+            rospy.loginfo(self.q.T)
+            rospy.loginfo(r_ref - r)
+            self.r.sleep()
 
     def trans_m(self, a, alpha, d, theta):
         m = np.array([[np.cos(theta), -np.sin(theta), 0., a],
@@ -80,7 +70,8 @@ if __name__ == '__main__':
     try:
         rospy.init_node('vr_controller')
         pose_pub = Pose_pub()
+        pose_pub.ik()
         rospy.spin()
 
-    except rospy.ROSIntteruptException:
+    except rospy.ROSInterruptException:
         pass
